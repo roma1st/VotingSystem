1929d38e9bb58b831527e59b475810be1a7240a2
RomanStroziuk <roman.stroziuk@oa.edu.ua>
2026-04-28 14:21:27 +0300

    chore: initial project setup and CI/CD

---
+++ VotingSystem.IntegrationTests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace VotingSystem.IntegrationTests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ VotingSystem.UnitTests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace VotingSystem.UnitTests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
